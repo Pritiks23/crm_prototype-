21a843ed773f062cf86ae871d4ab94c25c4faf53
Pritiks23 <65793273+Pritiks23@users.noreply.github.com>
2026-02-27 08:52:38 -0800

    Create schemas.py

---
+++ schemas.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+from typing import Dict, Optional
+
+class ContactCreate(BaseModel):
+    tenant_id: str
+    email: str
+    properties: Dict
+
+class ContactUpdate(BaseModel):
+    properties: Optional[Dict]
+
+class ActivityCreate(BaseModel):
+    type: str
+    metadata: Dict
